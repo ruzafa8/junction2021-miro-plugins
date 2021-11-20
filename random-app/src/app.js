5a70078bb51f5a689ae88dfd223a846ca487920b
Andres Morilla <weamalisima@gmail.com>
2021-11-20 18:27:36 +0100

    random42

--- random-app/src/app.js
+++ random-app/src/app.js
@@ -1,12 +1,5 @@
 const { board } = window.miro;
 
-async function init() {
-  const stickyNote = await board.createStickyNote({
-    content: "Hello, World!",
-  });
-
-  await board.viewport.zoomTo(stickyNote);
-}
 
 document.getElementById("button").onclick=async function(){
   var min=parseInt(document.getElementById("min_num").value);
@@ -35,5 +28,3 @@ document.getElementById("button").onclick=async function(){
     console.log(e)
   }
 }
-
-init();
