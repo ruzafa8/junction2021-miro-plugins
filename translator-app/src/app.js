d39624e87a72659374f23551ba8807d82467c7cb
ruzafa8 <ruzafa8@gmail.com>
2021-11-20 13:19:45 +0100

    Added support for all available languages (dynamically obtained from deepl REST API)

--- translator-app/src/app.js
+++ translator-app/src/app.js
@@ -10,3 +10,35 @@ document.getElementById("button").onclick = () => {
     document.getElementById("result").value = res.translations[0].text;
   }).catch(console.error)
 }
+
+let supportedLanguages = [];
+
+const retrieveLanguage = () => {
+  fetch(`https://api-free.deepl.com/v2/languages?auth_key=${env.DEEPL_API_KEY}&type=target`)
+  .then(res => res.text())
+  .then(JSON.parse)
+  .then(res => {
+    supportedLanguages = res;
+    setLanguages(res)
+  }).catch(console.error);
+}
+
+const setLanguages = languages => {
+  const select = document.getElementById('languageSelector');
+  console.log(languages);
+  languages.sort((item1,item2) => item1.name > item2.name ? 1 : item1.name == item2.name ? 0 : -1)
+  console.log(languages);
+
+  const children = languages.map(({language, name}) => {
+    const opt = document.createElement('option');
+    opt.value = language;
+    opt.innerHTML = name;
+    if (language == "EN-GB") opt.selected = true;
+    return opt;
+  });
+
+
+  select.replaceChildren(...children);
+}
+
+retrieveLanguage();
