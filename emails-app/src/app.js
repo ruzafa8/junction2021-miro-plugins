8f7dbec9b8c5efd03cc240313dcdee60ecef042d
ruzafa8 <ruzafa8@gmail.com>
2021-11-20 19:29:07 +0100

    Add simple table with stickers

--- emails-app/src/app.js
+++ emails-app/src/app.js
@@ -1,5 +1,19 @@
 const { board } = window.miro;
 
-document.getElementById("button").onclick=async function(){
+const columnWidth = 150;
+const rowHeight = 100;
+const fontSize = 36;
 
+const stickers = [];
+document.getElementById("btn").onclick = () => {
+  for(let x = 0; x < 3; x++) {
+    for(let y = 0; y < 3; y++) {
+      board.createStickyNote({
+        x:x*columnWidth,
+        y:y*rowHeight + 50*y,
+        width:columnWidth,
+        height:rowHeight
+      }).then(sticker => stickers.append(sticker))
+    }
+  }
 }
