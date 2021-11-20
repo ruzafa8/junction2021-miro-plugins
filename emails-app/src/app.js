b34395d4e5a5dd8a15f686ae0145bc6af58830ec
ruzafa8 <ruzafa8@gmail.com>
2021-11-20 20:42:28 +0100

    Changes emali

--- emails-app/src/app.js
+++ emails-app/src/app.js
@@ -5,15 +5,29 @@ const rowHeight = 100;
 const fontSize = 36;
 
 const stickers = [];
-document.getElementById("btn").onclick = () => {
+document.getElementById("btn").onclick = async () => {
   for(let x = 0; x < 3; x++) {
     for(let y = 0; y < 3; y++) {
-      board.createStickyNote({
+      console.log("creating sticker at", x, y)
+      stickers.push({x,y,id:(await board.createStickyNote({
         x:x*columnWidth,
         y:y*rowHeight + 50*y,
         width:columnWidth,
         height:rowHeight
-      }).then(sticker => stickers.append(sticker))
+      })).id})
+      console.log("created sticker at", x, y)
+
     }
   }
+  console.log(stickers);
+}
+document.getElementById("read").onclick = () => {
+  const x = parseInt(document.getElementById("x").value);
+  const y = parseInt(document.getElementById("y").value);
+  console.log(x,y);
+  const {id} = stickers.find(sticker => sticker.x == x && sticker.y == y);
+  console.log(id);
+  board.getById(id).then(sticker => {
+    console.log(sticker.content);
+  })
 }
