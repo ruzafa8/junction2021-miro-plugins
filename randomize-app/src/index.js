7edc570b19afe8bf95aecde38c6e875cc0dbd0c9
Andres Morilla <weamalisima@gmail.com>
2021-11-20 13:55:02 +0100

    delete

--- randomize-app/src/index.js
+++
@@ -1,9 +0,0 @@
-const { board } = window.miro;
-
-async function init() {
-  await board.ui.on("icon:click", async () => {
-    await board.ui.openPanel({ pageUrl: "app.html" });
-  });
-}
-
-init();
